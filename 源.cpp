63bf7af99378df1312a47b5726996a758f4883e8
haihang <2212387384@qq.com>
2018-02-05 22:02:50 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,40 @@
+#include <string.h>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+const int N = 10000000 + 10;
+bool prime[N];
+int hs[664579 + 10];
+void getPrimeTable()
+{
+	for (int i = 3; i <= 3333; i += 2)
+	{
+		if (prime[i] == 0)
+		for (int j = i*i; j < N; j += 2 * i)
+			prime[j] = 1;
+	}
+	int total = 1;
+	hs[1] = 2;
+	for (int i = 3; i < N; ++i)
+	{
+		if (i % 2 != 0 && prime[i] == 0)
+		{
+
+			hs[++total] = i;
+		}
+	}
+}
+int main()
+{
+	getPrimeTable();
+	int n, i;
+	while (scanf("%d", &n)&&n)
+	{
+		for (i = 1; i <= 664579; ++i)
+		if (hs[i] > n && hs[i - 1] <= n)
+			break;
+		printf("%d\n", i - 1);
+	}
+	return 0;
+}
